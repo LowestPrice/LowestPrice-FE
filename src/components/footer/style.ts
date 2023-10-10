3cedc5afb582dbba98ba0d2200eed2868f201912
Seoyeong Min <mseo1123@naver.com>
2023-10-10 09:35:52 +0900

    Design: 매거진 페이지(메인/상세), navbar 뼈대 구성

---
+++ src/components/footer/style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const FlexBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  border-top: 1px solid grey;
+`;
+
+export const ButtonStyle = styled.button`
+  background-color: transparent;
+  width: 52px;
+  height: 52px;
+  font-size: 11px;
+  margin: 6px 0px 20px 0px;
+  border: none;
+`;
