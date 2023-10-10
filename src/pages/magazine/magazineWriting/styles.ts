9a88bcf8b0d0832a34471385b201cfdd8358cb8b
Seoyeong Min <mseo1123@naver.com>
2023-10-10 23:04:00 +0900

    Design: 매거진 작성 페이지 뼈대 완료

---
+++ src/pages/magazine/magazineWriting/styles.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const FlexBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Button = styled.button`
+  background-color: transparent;
+  border: none;
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+export const ContentBox = styled.div`
+  width: 335px;
+  height: 527px;
+  margin: 50px auto 0px auto;
+`;
+
+export const Title = styled.div`
+  font-size: 26px;
+  font-weight: 500;
+  color: #b1b1b1;
+  padding-bottom: 15px;
+  border-bottom: 1px solid #b1b1b1;
+`;
+
+export const Content = styled.div`
+  font-size: 18px;
+  font-weight: 500;
+  color: #b1b1b1;
+  margin-top: 10px;
+`;
