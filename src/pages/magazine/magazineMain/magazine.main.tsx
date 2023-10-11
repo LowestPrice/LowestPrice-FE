225917a2af45c7ce208147ae18e8ea899f4373fd
Seoyeong Min <mseo1123@naver.com>
2023-10-11 22:44:52 +0900

    Design: 매거진 작성 페이지 수정

--- src/pages/magazine/magazineMain/magazine.main.tsx
+++ src/pages/magazine/magazineMain/magazine.main.tsx
@@ -1,14 +1,17 @@
+import { useNavigate } from 'react-router-dom';
 import { MagazineProps } from '../../../type/type';
-import { Container, Header, Title, Line, Subtitle, Box, Img, BoxPadding, BoxTitle, Flex, Content } from './styles';
+import { Container, Header, Title, Line, Subtitle, Box, Img, BoxPadding, BoxTitle, Flex, Content, Writing } from './styles';
 
 const Magazine: React.FC<MagazineProps> = ({ data }) => {
+  const navigate = useNavigate();
   return (
     <Container>
       <Header>매거진</Header>
       <Line></Line>
       <Title>Apple 트렌드</Title>
       <Subtitle>IT 트렌드, 여기서 볼 수 있어요</Subtitle>
-      <Box>
+      <Writing onClick={() => navigate('/magazineWriting')}>글쓰기</Writing>
+      <Box onClick={() => navigate('/magazineDetail')}>
         <Img>img</Img>
         <BoxPadding>
           <BoxTitle>{data.title}</BoxTitle>
--- src/pages/magazine/magazineWriting/magazine.writing.tsx
+++ src/pages/magazine/magazineWriting/magazine.writing.tsx
@@ -1,16 +1,50 @@
-import { FlexBox, Button, ContentBox, Title, Content } from './styles';
+import React, { useState } from 'react';
+import { FlexBox, Button, ContentBox, Title, Content, DirectionCol, PhotoAdd, PhotoDiv } from './styles';
+import { useNavigate } from 'react-router-dom';
 
-const MagazineWriting = () => {
+const MagazineWriting: React.FC = () => {
+  const navigate = useNavigate();
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [saveContent, setSaveContent] = useState<unknown[]>([]);
 
+  const onTitleChangeHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    console.log(e.target.value);
+    setTitle(e.target.value);
+  };
+
+  const onContentChangeHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    console.log(e.target.value);
+    setContent(e.target.value);
+  };
+
+  const onSubmitButtonHandler = () => {
+    const newContent = {
+      title,
+      content,
+    };
+
+    setSaveContent([...saveContent, newContent]);
+    setTitle('');
+    setContent('');
+    console.log(setSaveContent);
+  };
   return (
     <>
       <FlexBox>
-        <Button>□</Button>
-        <Button>등록</Button>
+        <Button onClick={() => navigate('/magazine')} back-color={'#b1b1b1'} width={'24px'}></Button>
+        <Button onClick={onSubmitButtonHandler} back-color={'transparent'} width={'80px'}>
+          등록
+        </Button>
       </FlexBox>
       <ContentBox>
-        <Title>제목</Title>
-        <Content>내용을 입력하세요.</Content>
+        <DirectionCol>
+          <PhotoDiv>
+            <PhotoAdd></PhotoAdd>
+          </PhotoDiv>
+          <Title type='text' placeholder='제목' onChange={onTitleChangeHandler} />
+          <Content type='text' placeholder='내용을 입력하세요' onChange={onContentChangeHandler} />
+        </DirectionCol>
       </ContentBox>
     </>
   );
