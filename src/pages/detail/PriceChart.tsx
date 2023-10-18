dee17ec9ca590157a3757d17a20ebaf526d872ec
Seoyeong Min <mseo1123@naver.com>
2023-10-18 20:32:09 +0900

    Docs: 그래프 기능 수정

--- src/pages/detail/PriceChart.tsx
+++ src/pages/detail/PriceChart.tsx
@@ -3,6 +3,8 @@ import { Line } from 'react-chartjs-2';
 import { useQuery } from 'react-query';
 import { getPriceHistory } from '../../api/product';
 import Chart from 'chart.js/auto';
+import annotationPlugin from 'chartjs-plugin-annotation';
+Chart.register(annotationPlugin);
 import { CategoryScale } from 'chart.js';
 Chart.register(CategoryScale);
 
@@ -14,12 +16,14 @@ interface PriceData {
 
 interface ChartData {
   labels: string[];
-  datasets: {
-    label: string;
-    data: number[];
-    borderColor: string;
-    borderWidth: number;
-  }[];
+  datasets: [
+    {
+      label: string;
+      data: number[];
+      borderColor: string;
+      borderWidth: number;
+    }
+  ];
 }
 
 interface ParamsProps {
@@ -27,18 +31,15 @@ interface ParamsProps {
 }
 
 export const PriceChart: React.FC<ParamsProps> = ({ id }) => {
-  console.log(id, 'id값이 나올까?');
   const { isLoading, isError, data } = useQuery<PriceData | undefined>('priceHistory', () => getPriceHistory(id));
-  console.log(data, '데이터 결과');
-  console.log(data?.maxPrice, 'maxprice');
-  console.log(data?.minPrice, 'maxprice');
-  console.log(data?.priceHistoryForWeek, 'pricehistoryforweek');
+  const [chartOptions, setChartOptions] = useState<any>(null);
+  console.log(data, 'data');
 
   const [priceData, setPriceData] = useState<ChartData>({
     labels: [],
     datasets: [
       {
-        label: 'Lowest Price',
+        label: 'price history',
         data: [],
         borderColor: 'black',
         borderWidth: 2,
@@ -51,11 +52,52 @@ export const PriceChart: React.FC<ParamsProps> = ({ id }) => {
       const labels = Object.keys(data.priceHistoryForWeek);
       const datasetData = Object.values(data.priceHistoryForWeek) as number[];
 
+      const minPrice = Math.min(...datasetData);
+      const maxPrice = Math.max(...datasetData);
+
+      // x축에서 최솟값과 최댓값의 위치 찾기
+      const xValueForMin = labels[datasetData.indexOf(minPrice)];
+      const xValueForMax = labels[datasetData.indexOf(maxPrice)];
+
+      setChartOptions({
+        plugins: {
+          annotation: {
+            drawTime: 'afterDatasetsDraw',
+            annotations: {
+              minLabel: {
+                type: 'line',
+                scaleID: 'y', // y축에 위치하게 함
+                value: minPrice,
+                // borderColor: 'red',
+                // borderWidth: 1,
+                label: {
+                  enabled: true,
+                  content: `최솟값: ${minPrice}`,
+                  position: 'end',
+                },
+              },
+              maxLabel: {
+                type: 'line',
+                scaleID: 'y', // y축에 위치하게 함
+                value: maxPrice,
+                // borderColor: 'green',
+                // borderWidth: 1,
+                label: {
+                  enabled: true,
+                  content: `최댓값: ${maxPrice}`,
+                  position: 'start',
+                },
+              },
+            },
+          },
+        },
+      });
+
       setPriceData({
         labels: labels,
         datasets: [
           {
-            label: 'Price History',
+            label: '',
             data: datasetData,
             borderColor: 'black',
             borderWidth: 2,
@@ -65,13 +107,8 @@ export const PriceChart: React.FC<ParamsProps> = ({ id }) => {
     }
   }, [data]);
 
-  if (isLoading) {
-    return <h1>로딩중입니다</h1>;
-  }
+  if (isLoading) return <h1>로딩중입니다</h1>;
+  if (isError) return <h1>에러가 발생했습니다.</h1>;
 
-  if (isError) {
-    return <h1>에러가 발생했습니다.</h1>;
-  }
-
-  return <Line data={priceData} />;
+  return <Line data={priceData} options={chartOptions || {}} />;
 };
