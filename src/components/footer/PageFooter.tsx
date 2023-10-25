9b6d49a41012c403ea495c5b03c234deb7ce58be
jinhyeok99 <143480840+yngjnhykk@users.noreply.github.com>
2023-10-25 15:22:27 +0900

    Fix: footer merge

--- src/components/footer/PageFooter.tsx
+++ src/components/footer/PageFooter.tsx
@@ -1,11 +1,38 @@
 import { useNavigate } from 'react-router-dom';
-import { FlexBox, ButtonStyle, Text } from './style';
-import { MyPageIcon, BellIcon, FooterMagazineIcon, HomeIcon } from '../../assets/icon/icon';
 import Cookies from 'js-cookie';
+import {
+  MyPageIcon,
+  BellIcon,
+  FooterMagazineIcon,
+  HomeIcon,
+  ColorMyPageIcon,
+  ColorBellIcon,
+  ColorFooterMagazineIcon,
+  ColorHomeIcon,
+} from '../../assets/icon/icon';
 import { toast } from 'react-toastify';
+import styled from 'styled-components';
+import { useLocation } from 'react-router-dom';
+
+import { useState, useEffect, ButtonHTMLAttributes } from 'react';
+
+interface ButtonStyleProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  $active?: boolean;
+}
 
 const PageFooter = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const [activeIcon, setActiveIcon] = useState('');
+
+  useEffect(() => {
+    setActiveIcon(location.pathname);
+  }, [location.pathname]);
+
+  const handleNavigation = (path: any) => {
+    navigate(path);
+    setActiveIcon(path);
+  };
 
   const accessToken = Cookies.get('Authorization');
 
@@ -31,20 +58,20 @@ const PageFooter = () => {
 
   return (
     <FlexBox>
-      <ButtonStyle onClick={() => navigate('/')}>
-        <HomeIcon></HomeIcon>
+      <ButtonStyle $active={location.pathname === '/'} onClick={() => handleNavigation('/')}>
+        {activeIcon === '/' ? <ColorHomeIcon /> : <HomeIcon />}
         <Text>홈</Text>
       </ButtonStyle>
-      <ButtonStyle onClick={() => navigate('/magazine')}>
-        <FooterMagazineIcon></FooterMagazineIcon>
+      <ButtonStyle $active={location.pathname === '/magazine'} onClick={() => handleNavigation('/magazine')}>
+        {activeIcon === '/magazine' ? <ColorFooterMagazineIcon /> : <FooterMagazineIcon />}
         <Text>매거진</Text>
       </ButtonStyle>
-      <ButtonStyle onClick={handleAlarmButton}>
-        <BellIcon></BellIcon>
+      <ButtonStyle $active={location.pathname === '/notification'} onClick={handleAlarmButton}>
+        {activeIcon === '/notification' ? <ColorBellIcon /> : <BellIcon />}
         <Text>내 알림</Text>
       </ButtonStyle>
-      <ButtonStyle onClick={handleMypageButton}>
-        <MyPageIcon></MyPageIcon>
+      <ButtonStyle $active={location.pathname === '/mypage'} onClick={handleMypageButton}>
+        {activeIcon === '/mypage' ? <ColorMyPageIcon /> : <MyPageIcon />}
         <Text>마이페이지</Text>
       </ButtonStyle>
     </FlexBox>
@@ -52,3 +79,35 @@ const PageFooter = () => {
 };
 
 export default PageFooter;
+
+export const FlexBox = styled.div`
+  width: 375px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  border-top: 1px solid grey;
+  position: fixed;
+  bottom: 0;
+  background-color: white;
+  z-index: 1000;
+  margin-top: 5px;
+`;
+
+export const ButtonStyle = styled.button<ButtonStyleProps>`
+  background-color: transparent;
+  width: 52px;
+  height: 52px;
+  font-size: 11px;
+  margin: 6px 0px 6px 0px;
+  border: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: ${(props) => (props.$active ? '#00ABF9' : '#6F6F6F')};
+`;
+
+export const Text = styled.div`
+  font-size: 11px;
+  width: 48px;
+`;
