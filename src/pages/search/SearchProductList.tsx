f54fd60c5f069fd82d34ae3ae6a6bc0d925eef4d
jinhyeok99 <143480840+yngjnhykk@users.noreply.github.com>
2023-10-31 20:27:28 +0900

    Feat: 무한스크롤 hasNextPage

--- src/pages/search/SearchProductList.tsx
+++ src/pages/search/SearchProductList.tsx
@@ -1,11 +1,11 @@
-import { useQueries } from 'react-query';
 import SearchProduct from './SearchProduct';
 import styled from 'styled-components';
-import { getSearch, getFilteredSearch } from '../../api/product';
 import { useEffect } from 'react';
 import { Product } from '../../type';
 import Loading from '../../components/Loading';
 import Error from '../../components/Error';
+import { infiniteSearch } from '../../infiniteQueries/search';
+import Observer from '../../components/Observer';
 
 interface Props {
   searchWord: string | undefined;
@@ -18,37 +18,45 @@ interface Props {
 function SearchProductList(props: Props) {
   // 검색한 상품과 검색한 상품을 필터링한 데이터 불러오기 -----------------------------------------------------------------
 
-  const result = useQueries([
-    { queryKey: ['searchProduct', props.searchWord], queryFn: () => getSearch(props.searchWord, props.isSoldout) },
-    {
-      queryKey: ['FilteredSearchProduct'],
-      queryFn: () => getFilteredSearch(props.filterName, props.searchWord, props.isSoldout),
-      enabled: !!props.filterButton,
-    },
-  ]);
+  // const result = useQueries([
+  //   { queryKey: ['searchProduct', props.searchWord], queryFn: () => getSearch(props.searchWord, props.isSoldout) },
+  //   {
+  //     queryKey: ['FilteredSearchProduct'],
+  //     queryFn: () => getFilteredSearch(props.filterName, props.searchWord, props.isSoldout),
+  //     enabled: !!props.filterButton,
+  //   },
+  // ]);
+
+  const infiniteSearchData = infiniteSearch(props.searchWord, props.isSoldout);
 
   // 필터버튼 클릭할 때마다 리패치 -------------------------------------------------------------------------
 
-  useEffect(() => {
-    result[0].refetch();
-    result[1].refetch();
-  }, [props.filterButton, props.isFilter]);
+  useEffect(() => {}, [props.filterButton, props.isFilter]);
 
   // 로딩, 에러 관리 ---------------------------------------------------------
 
-  if (result[0].status === 'loading') {
+  if (infiniteSearchData.status === 'loading') {
     return <Loading />;
   }
-  if (result[0].status === 'error') {
+  if (infiniteSearchData.status === 'error') {
     return <Error />;
   }
 
-  if (result[1].status === 'loading') {
-    return <Loading />;
-  }
-  if (result[1].status === 'error') {
-    return <Error />;
-  }
+  // if (result[1].status === 'loading') {
+  //   return <Loading />;
+  // }
+  // if (result[1].status === 'error') {
+  //   return <Error />;
+  // }
+
+  const infiniteSearchDataList: Product[] = infiniteSearchData.data?.pages.reduce(function (acc, cur) {
+    return acc.concat(cur);
+  });
+
+  const handleIntersection = () => {
+    infiniteSearchData.fetchNextPage();
+    // infiniteCategoryData.fetchNextPage();
+  };
 
   return (
     <div>
@@ -57,9 +65,10 @@ function SearchProductList(props: Props) {
           ? result[1].data.map((item: Product, index: number) => {
               return <SearchProduct key={index} {...item} />;
             })
-          : result[0].data.map((item: Product, index: number) => {
+          : infiniteSearchDataList.map((item: Product, index: number) => {
               return <SearchProduct key={index} {...item} />;
             })}
+        <Observer handleIntersection={handleIntersection} />
       </Wrap>
     </div>
   );
