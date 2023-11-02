28f5d695424979068774df545c3d998ab7282a5d
jinhyeok99 <143480840+yngjnhykk@users.noreply.github.com>
2023-11-02 16:43:37 +0900

    Update login.ts

--- src/api/login.ts
+++ src/api/login.ts
@@ -6,7 +6,7 @@ import axios from 'axios';
 export const loginWithKakao = () => {
   const SERVER_URL = `${import.meta.env.VITE_API_KEY}` || 'http://localhost:5173';
   const kakaoOauthURL = `https://kauth.kakao.com/oauth/authorize?response_type=code&redirect_uri=${encodeURIComponent(
-    `${SERVER_URL}/api/kakao/temporary-login`
+    `${SERVER_URL}/api/kakao/callback`
   )}&client_id=1b3c349efac233223cb5b44ca84c0ff6`;
   window.location.href = kakaoOauthURL;
 };
@@ -29,7 +29,6 @@ export const DeleteIdWithKakao = async () => {
   console.log(accessToken, '탈퇴 토큰');
   try {
     const response = await axios.delete(`${import.meta.env.VITE_API_KEY}/kakao/deactivate`, { headers: { Authorization: accessToken } });
-    document.cookie = 'Authorization=; max-age=0; path=/';
     return response;
   } catch (error) {
     console.error('회원 탈퇴 에러', error);
