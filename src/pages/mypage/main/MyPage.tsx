682fc792aa7d8dd170c3e7e7a4534b8cc99ecfef
jinhyeok99 <143480840+yngjnhykk@users.noreply.github.com>
2023-11-03 11:20:47 +0900

    Fix: 로그아웃

--- src/pages/mypage/main/MyPage.tsx
+++ src/pages/mypage/main/MyPage.tsx
@@ -1,9 +1,9 @@
 import { useNavigate } from 'react-router-dom';
-import { useQuery } from 'react-query';
+import { useMutation, useQuery } from 'react-query';
 import styled from 'styled-components';
 
 import { getUserinfo } from '../../../api/mypage';
-// import { postlogout } from '../../../api/login';
+import { postLogout } from '../../../api/login';
 
 import PageFooter from '../../../components/footer/PageFooter';
 import Loading from '../../../components/Loading';
@@ -22,14 +22,14 @@ function Mypage() {
   const { data, status } = useQuery('userInfo', getUserinfo);
 
   // 로그아웃하기 -----------------------------------------
-  // const logoutMutation = useMutation(postlogout, {
-  //   onSuccess: () => {
-  //     console.log('로그아웃 성공');
-  //   },
-  //   onError: () => {
-  //     console.log('로그아웃 실패');
-  //   },
-  // });
+  const logoutMutation = useMutation(postLogout, {
+    onSuccess: () => {
+      console.log('로그아웃 성공');
+    },
+    onError: () => {
+      console.log('로그아웃 실패');
+    },
+  });
 
   if (status === 'loading') {
     return <Loading />;
@@ -41,9 +41,7 @@ function Mypage() {
   // 로그아웃--------------------------------------------------------
 
   const handleLogoutButton = () => {
-    // logoutMutation.mutate();
-    Cookies.remove('Authorization');
-    navigate('/');
+    logoutMutation.mutate();
   };
 
   const accessToken = Cookies.get('Authorization');
