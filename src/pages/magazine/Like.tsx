34ce7d29b206b8dfba7e0e386fc5089506b3b76d
Seoyeong Min <mseo1123@naver.com>
2023-11-08 10:24:39 +0900

    Design: 공유하기 푸터 수정, 좋아요 pop 애니메이션

--- src/pages/magazine/Like.tsx
+++ src/pages/magazine/Like.tsx
@@ -1,23 +1,52 @@
-import styled from 'styled-components';
-import { LikeProps } from '../../type/type';
+import styled, { keyframes, css } from 'styled-components';
 import { ColorHeartIcon, LineHeartIcon } from '../../assets/icon/icon';
-import { HeartProps } from '../../type/type';
+import { LikeProps, HeartProps } from '../../type/type';
+import { ButtonHTMLAttributes } from 'react';
+
+interface HeartButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  like: boolean;
+}
+
+const pop = keyframes`
+  0% { transform: scale(1); }
+  50% { transform: scale(1.2); }
+  100% { transform: scale(1); }
+`;
+
+// const flyAndFade = keyframes`
+//   0% {
+//     transform: translateY(0);
+//     /* opacity: 1; */
+//   }
+//   50% {
+//     transform: translateY(-10rem);
+//     /* opacity: 0; */
+//   }
+// `;
 
 const Heart: React.FC<HeartProps> = ({ like, onClick }) => {
   return (
-    <button
-      onClick={onClick}
-      style={{
-        marginTop: '-0.25rem',
-        background: 'none',
-        border: 'none',
-        outline: 'none',
-      }}
-    >
+    <HeartButton like={like} onClick={onClick}>
       {like ? <ColorHeartIcon /> : <LineHeartIcon />}
-    </button>
+    </HeartButton>
   );
 };
+
+// const Heart: React.FC<HeartProps> = ({ like, onClick }) => {
+//   return (
+//     <button
+//       onClick={onClick}
+//       style={{
+//         marginTop: '-0.25rem',
+//         background: 'none',
+//         border: 'none',
+//         outline: 'none',
+//       }}
+//     >
+//       {like ? <ColorHeartIcon /> : <LineHeartIcon />}
+//     </button>
+//   );
+// };
 
 const Like: React.FC<LikeProps> = ({ isLiked, magazineId, likeCount, handleLikeClick, index }) => {
   return (
@@ -36,4 +65,18 @@ const LikeFlex = styled.div`
   display: flex;
   justify-content: flex-end;
   margin-right: 0.9375rem;
+`;
+
+const HeartButton = styled.button<HeartButtonProps>`
+  margin-top: -0.25rem;
+  background: none;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  animation: ${(props) =>
+    props.like
+      ? css`
+          ${pop} 0.6s ease forwards
+        `
+      : 'none'};
 `;
